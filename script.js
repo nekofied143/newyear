49f040c842af446223e3fe058a85df9f5d2a5b1e
nekofied143 <112548898+nekofied143@users.noreply.github.com>
2023-12-12 01:10:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ function updateCountdown() {
    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
-   document.getElementById('year').innerText = currentYear;
-   document.getElementById('days').innerText = days + 'D';
-   document.getElementById('hours').innerText = hours + 'H';
-   document.getElementById('minutes').innerText = minutes + 'M';
+   document.getElementById('year').innerText = currentYear + '|';
+   document.getElementById('days').innerText = days + 'D' + '|';
+   document.getElementById('hours').innerText = hours + 'H' + '|';
+   document.getElementById('minutes').innerText = minutes + 'M' + '|';
    document.getElementById('seconds').innerText = seconds + 'S';
 
    if (difference <= 0) {
